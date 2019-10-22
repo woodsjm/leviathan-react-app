e7244496177e938b7624eb09ace855630979b27f
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2019-10-22 15:34:02 -0500

    Organized all basic wall tile images

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Level1 from './Levels/Level-1.js'
 // Leviathan Front-End
 
 function App() {
-  return (
-    <div className="App">
-      <Level1 />
-    </div>
-  );
+
+    return (
+        <div className="App">
+          <Level1 />
+        </div>
+    );
 }
 
 export default App;
