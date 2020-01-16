067ff351d177c37f0271683488e7c020c398ff09
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2020-01-16 01:00:02 -0500

    Styled login form

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
         this.state = {
             password: '',
             email: '',
-            loggedIn: true
+            loggedIn: false
         }
     }
 
--- src/Login/index.js
+++ src/Login/index.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import './login.css'
+import '../GameContainer/game-container.css'
 
 const Login = () => {
     return(
-        <div> Hello</div>
-        )
+        <div>
+            <div>HELLO</div>
+
+
+
+            <div className="main" augmented-ui="r-clip bl-clip exe">
+                <p className="sign glitch" align="center">Ship Login</p>
+
+                <form className="form1">
+                    <div>
+                        <div>
+                            <input className="un" type="text"  placeholder="Email" />
+                        </div>
+                        <div>
+                            <input className="pass" type="password"  placeholder="Password" />
+                        </div>
+                      <a className="submit" >Login</a>
+                      <p className="forgot" align="center"><a href="#"></a>Forgot Password?</p>
+                    
+                    </div>
+                </form>           
+            </div>
+
+        </div>
+
+    )
 }
 
 export default Login
